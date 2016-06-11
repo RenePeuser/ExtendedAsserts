7d2f500fb943070ceb110ad5de062b275c975e8b
RenePeuser - Rene Peuser <rene.peuser@hotmail.de>
2016-06-11 12:32:04 +0200

    startup

---
+++ ExtendedAsserts/ExtendedAsserts/AssertToObject.cs
@@ -0,0 +1,226 @@
+﻿using System;
+using System.Diagnostics.Contracts;
+using Extensions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ExtendedAsserts
+{
+    public static partial class Asserts
+    {
+        public static void IsNotPublic(object source)
+        {
+            Contract.Requires(source.IsNotNull());
+            Assert.IsTrue(source.IsNotPublic());
+        }
+
+        public static void IsPublic(object source)
+        {
+            Contract.Requires(source.IsNotNull());
+            Assert.IsTrue(source.IsNotPublic());
+        }
+
+        public static void IsNestedPublic(object source)
+        {
+            Contract.Requires(source.IsNotNull());
+            Assert.IsTrue(source.IsNestedPublic());
+        }
+
+        public static void IsNestedPrivate(object source)
+        {
+            Contract.Requires(source.IsNotNull());
+            Assert.IsTrue(source.IsNestedPrivate());
+        }
+
+        public static void IsNestedFamily(object source)
+        {
+            Contract.Requires(source.IsNotNull());
+            Assert.IsTrue(source.IsNestedFamily());
+        }
+
+        public static void IsNestedAssembly(object source)
+        {
+            Contract.Requires(source.IsNotNull());
+            Assert.IsTrue(source.IsNestedAssembly());
+        }
+
+        public static void IsNestedFamAndAssem(object source)
+        {
+            Contract.Requires(source.IsNotNull());
+            Assert.IsTrue(source.IsNestedFamAndAssem());
+        }
+
+        public static void IsNestedFamOrAssem(object source)
+        {
+            Contract.Requires(source.IsNotNull());
+            Assert.IsTrue(source.IsNestedFamOrAssem());
+        }
+
+        public static void IsAutoLayout(object source)
+        {
+            Contract.Requires(source.IsNotNull());
+            Assert.IsTrue(source.IsAutoLayout());
+        }
+
+        public static void IsLayoutSequential(object source)
+        {
+            Contract.Requires(source.IsNotNull());
+            Assert.IsTrue(source.IsLayoutSequential());
+        }
+
+        public static void IsExplicitLayout(object source)
+        {
+            Contract.Requires(source.IsNotNull());
+            Assert.IsTrue(source.IsExplicitLayout());
+        }
+
+        public static void IsClass(object source)
+        {
+            Contract.Requires(source.IsNotNull());
+            Assert.IsTrue(source.IsClass());
+        }
+
+        public static void IsInterface(object source)
+        {
+            Contract.Requires(source.IsNotNull());
+            Assert.IsTrue(source.IsInterface());
+        }
+
+        public static void IsValueType(object source)
+        {
+            Contract.Requires(source.IsNotNull());
+            Assert.IsTrue(source.IsValueType());
+        }
+
+        public static void IsAbstract(object source)
+        {
+            Contract.Requires(source.IsNotNull());
+            Assert.IsTrue(source.IsAbstract());
+        }
+
+        public static void IsSealed(object source)
+        {
+            Contract.Requires(source.IsNotNull());
+            Assert.IsTrue(source.IsSealed());
+        }
+
+        public static void IsEnum(object source)
+        {
+            Contract.Requires(source.IsNotNull());
+            Assert.IsTrue(source.IsEnum());
+        }
+
+        public static void IsSpecialName(object source)
+        {
+            Contract.Requires(source.IsNotNull());
+            Assert.IsTrue(source.IsSpecialName());
+        }
+
+        public static void IsImport(object source)
+        {
+            Contract.Requires(source.IsNotNull());
+            Assert.IsTrue(source.IsImport());
+        }
+
+        public static void IsSerializable(object source)
+        {
+            Contract.Requires(source.IsNotNull());
+            Assert.IsTrue(source.IsSerializable());
+        }
+
+        public static void IsAnsiClass(object source)
+        {
+            Contract.Requires(source.IsNotNull());
+            Assert.IsTrue(source.IsAnsiClass());
+        }
+
+        public static void IsUnicodeClass(object source)
+        {
+            Contract.Requires(source.IsNotNull());
+            Assert.IsTrue(source.IsUnicodeClass());
+        }
+
+        public static void IsAutoClass(object source)
+        {
+            Contract.Requires(source.IsNotNull());
+            Assert.IsTrue(source.IsAutoClass());
+        }
+
+        public static void IsArray(object source)
+        {
+            Contract.Requires(source.IsNotNull());
+            Assert.IsTrue(source.IsArray());
+        }
+
+        public static void IsByRef(object source)
+        {
+            Contract.Requires(source.IsNotNull());
+            Assert.IsTrue(source.IsByRef());
+        }
+
+        public static void IsPointer(object source)
+        {
+            Contract.Requires(source.IsNotNull());
+            Assert.IsTrue(source.IsPointer());
+        }
+
+        public static void IsPrimitive(object source)
+        {
+            Contract.Requires(source.IsNotNull());
+            Assert.IsTrue(source.IsPrimitive());
+        }
+
+        public static void IsComObject(object source)
+        {
+            Contract.Requires(source.IsNotNull());
+            Assert.IsTrue(source.IsComObject());
+        }
+
+        public static void IsContextful(object source)
+        {
+            Contract.Requires(source.IsNotNull());
+            Assert.IsTrue(source.IsContextful());
+        }
+
+        public static void IsMarshalByRef(object source)
+        {
+            Contract.Requires(source.IsNotNull());
+            Assert.IsTrue(source.IsMarshalByRef());
+        }
+        public static void HasElementType(object source)
+        {
+            Contract.Requires(source.IsNotNull());
+            Assert.IsTrue(source.HasElementType());
+        }
+
+        public static void IsImmutable(object source)
+        {
+            Contract.Requires(source.IsNotNull());
+            Assert.IsTrue(source.IsImmutable());
+        }
+
+        public static void HasCustomAttribute<TAttribute>(object source) where TAttribute : Attribute
+        {
+            Contract.Requires(source.IsNotNull());
+            Assert.IsTrue(source.HasCustomAttribute<TAttribute>());
+        }
+
+        public static void IsTypeOf<T>(object source)
+        {
+            Contract.Requires(source.IsNotNull());
+            Assert.IsTrue(source.IsTypeOf<T>());
+        }
+
+        public static void IsSubClassOf<T>(object source) where T : class
+        {
+            Contract.Requires(source.IsNotNull());
+            Assert.IsTrue(source.IsSubClassOf<T>());
+        }
+
+
+        public static void IsInterfaceImplemented<T>(object source) where T : class
+        {
+            Contract.Requires(source.IsNotNull());
+            Assert.IsTrue(source.IsInterfaceImplemented<T>());
+        }
+    }
+}
---
+++ ExtendedAsserts/ExtendedAsserts/AssertToT.cs
@@ -0,0 +1,190 @@
+﻿using System;
+using Extensions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ExtendedAsserts
+{
+    public static partial class Asserts
+    {
+        public static void IsNotPublic<T>()
+        {
+            Assert.IsTrue(typeof(T).IsNotPublic());
+        }
+
+        public static void IsPublic<T>()
+        {
+            Assert.IsTrue(typeof(T).IsNotPublic());
+        }
+
+        public static void IsNestedPublic<T>()
+        {
+            Assert.IsTrue(typeof(T).IsNestedPublic());
+        }
+
+        public static void IsNestedPrivate<T>()
+        {
+            Assert.IsTrue(typeof(T).IsNestedPrivate());
+        }
+
+        public static void IsNestedFamily<T>()
+        {
+            Assert.IsTrue(typeof(T).IsNestedFamily());
+        }
+
+        public static void IsNestedAssembly<T>()
+        {
+            Assert.IsTrue(typeof(T).IsNestedAssembly());
+        }
+
+        public static void IsNestedFamAndAssem<T>()
+        {
+            Assert.IsTrue(typeof(T).IsNestedFamAndAssem());
+        }
+
+        public static void IsNestedFamOrAssem<T>()
+        {
+            Assert.IsTrue(typeof(T).IsNestedFamOrAssem());
+        }
+
+        public static void IsAutoLayout<T>()
+        {
+            Assert.IsTrue(typeof(T).IsAutoLayout());
+        }
+
+        public static void IsLayoutSequential<T>()
+        {
+            Assert.IsTrue(typeof(T).IsLayoutSequential());
+        }
+
+        public static void IsExplicitLayout<T>()
+        {
+            Assert.IsTrue(typeof(T).IsExplicitLayout());
+        }
+
+        public static void IsClass<T>()
+        {
+            Assert.IsTrue(typeof(T).IsClass());
+        }
+
+        public static void IsInterface<T>()
+        {
+            Assert.IsTrue(typeof(T).IsInterface());
+        }
+
+        public static void IsValueType<T>()
+        {
+            Assert.IsTrue(typeof(T).IsValueType());
+        }
+
+        public static void IsAbstract<T>()
+        {
+            Assert.IsTrue(typeof(T).IsAbstract());
+        }
+
+        public static void IsSealed<T>()
+        {
+            Assert.IsTrue(typeof(T).IsSealed());
+        }
+
+        public static void IsEnum<T>()
+        {
+            Assert.IsTrue(typeof(T).IsEnum());
+        }
+
+        public static void IsSpecialName<T>()
+        {
+            Assert.IsTrue(typeof(T).IsSpecialName());
+        }
+
+        public static void IsImport<T>()
+        {
+            Assert.IsTrue(typeof(T).IsImport());
+        }
+
+        public static void IsSerializable<T>()
+        {
+            Assert.IsTrue(typeof(T).IsSerializable());
+        }
+
+        public static void IsAnsiClass<T>()
+        {
+            Assert.IsTrue(typeof(T).IsAnsiClass());
+        }
+
+        public static void IsUnicodeClass<T>()
+        {
+            Assert.IsTrue(typeof(T).IsUnicodeClass());
+        }
+
+        public static void IsAutoClass<T>()
+        {
+            Assert.IsTrue(typeof(T).IsAutoClass());
+        }
+
+        public static void IsArray<T>()
+        {
+            Assert.IsTrue(typeof(T).IsArray());
+        }
+
+        public static void IsByRef<T>()
+        {
+            Assert.IsTrue(typeof(T).IsByRef());
+        }
+
+        public static void IsPointer<T>()
+        {
+            Assert.IsTrue(typeof(T).IsPointer());
+        }
+
+        public static void IsPrimitive<T>()
+        {
+            Assert.IsTrue(typeof(T).IsPrimitive());
+        }
+
+        public static void IsComObject<T>()
+        {
+            Assert.IsTrue(typeof(T).IsComObject());
+        }
+
+        public static void IsContextful<T>()
+        {
+            Assert.IsTrue(typeof(T).IsContextful());
+        }
+
+        public static void IsMarshalByRef<T>()
+        {
+            Assert.IsTrue(typeof(T).IsMarshalByRef());
+        }
+        public static void HasElementType<T>()
+        {
+            Assert.IsTrue(typeof(T).HasElementType());
+        }
+
+        public static void IsImmutable<T>()
+        {
+
+            Assert.IsTrue(typeof(T).IsImmutable());
+        }
+
+        public static void HasCustomAttribute<TSource, TAttribute>() where TSource : class where TAttribute : Attribute
+        {
+            Assert.IsTrue(typeof(TSource).HasCustomAttribute<TAttribute>());
+        }
+
+        public static void IsTypeOf<TSource, TExpected>()
+        {
+            Assert.IsTrue(typeof(TSource).IsTypeOf<TExpected>());
+        }
+
+        public static void IsSubClassOf<TSource, TSubClass>() where TSource : class where TSubClass : class
+        {
+            Assert.IsTrue(typeof(TSource).IsSubClassOf<TSubClass>());
+        }
+
+
+        public static void IsInterfaceImplemented<TSource, TInterface>() where TSource : class where TInterface : class
+        {
+            Assert.IsTrue(typeof(TSource).IsInterfaceImplemented<TInterface>());
+        }
+    }
+}
